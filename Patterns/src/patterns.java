8118fad9dd46f2c45a64da98eb2021984fd2f11f
SATHAR28 <sat20006@gmail.com>
2026-06-27 21:03:23 +0530

    Patterns program Started learning and worked some of it

---
+++ Patterns/src/patterns.java
@@ -0,0 +1,32 @@
+public class patterns {
+    public static void main(String[] args) {
+
+        pattern8(5);
+    }
+    static void pattern6( int n){
+        for (int row = 1; row <=n ; row++) {
+            int NoOfSpaces = n-row;
+            for (int s = 0; s < NoOfSpaces; s++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < row; col++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+    static void pattern8( int n){
+        for (int row = 1; row <=n ; row++) {
+            int NoOfSpaces = n-row;
+            for (int s = 0; s < NoOfSpaces; s++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < 2*row-1; col++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
