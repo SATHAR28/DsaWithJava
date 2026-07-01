f0c2d414b9753d9ae418b3a7ab8b7a973f18fe43
SATHAR28 <sat20006@gmail.com>
2026-07-01 12:12:25 +0530

    solved someof it

---
+++ Functions/GenralPgms/src/fibinocciseriesz.java
@@ -0,0 +1,25 @@
+public class fibinocciseriesz {
+    public static void main(String[] args) {
+
+        fibinocci(10);
+
+    }
+    static void fibinocci(int n){
+        int first = 0;
+        int second = 1;
+        System.out.print(first+" "+second+" ");
+
+        int temp =0;
+        for (int i = 0; i < n; i++) {
+
+
+            int next = first + second;
+
+            System.out.print(next+" ");
+            temp = first;
+            first = second;
+            second = next;
+
+        }
+    }
+}
