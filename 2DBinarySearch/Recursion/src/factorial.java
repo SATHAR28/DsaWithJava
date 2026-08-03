a2bf761613aea2bac7be2ecc5b1e2844f061d9ad
SATHAR28 <sat20006@gmail.com>
2026-08-04 01:01:21 +0530

    Fact using recursion done

--- 2DBinarySearch/Recursion/src/factorial.java
+++ 2DBinarySearch/Recursion/src/factorial.java
@@ -1,3 +1,20 @@
 public class factorial {
 
+    public static void main(String[] args) {
+        int n = 30;
+        System.out.println(fact(n));
+    }
+    static int fact (int n ){
+
+        if (n == 1){
+            return n ;
+        }
+
+
+        return n * fact(n-1);
+    }
+
+
+
+
 }
