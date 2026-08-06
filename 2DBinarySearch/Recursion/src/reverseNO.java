c38619e15f6bbdc15789c270abf687ea17e95567
SATHAR28 <sat20006@gmail.com>
2026-08-06 23:58:18 +0530

    reverse no using recursion

--- 2DBinarySearch/Recursion/src/reverseNO.java
+++ 2DBinarySearch/Recursion/src/reverseNO.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class reverseNO {
+    public static void main(String[] args) {
+        int  sum = 0;
+        int n = 8432;
+        System.out.println(reverse(n,sum));
+    }
+
+    static int reverse (int n ,int sum){
+
+        if(n == 0){
+            return sum;
+        }
+        int rem = n%10;
+         sum = sum*10+rem ;
+        return reverse(n/10,sum);
+    }
 }
