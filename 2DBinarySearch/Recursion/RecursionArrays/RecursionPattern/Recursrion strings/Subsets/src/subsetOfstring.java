66eb83720edf5a637bd7ef890a517f0ce110425f
SATHAR28 <sat20006@gmail.com>
2026-09-07 22:46:33 +0530

    contu to the prev commit But using Array List but didnt understand any thing

--- 2DBinarySearch/Recursion/RecursionArrays/RecursionPattern/Recursrion strings/Subsets/src/subsetOfstring.java
+++ 2DBinarySearch/Recursion/RecursionArrays/RecursionPattern/Recursrion strings/Subsets/src/subsetOfstring.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class subsetOfstring {
     public static void main(String[] args) {
 
@@ -5,23 +7,43 @@ public class subsetOfstring {
         String p = "";
 
         subsetOfstring o = new subsetOfstring();
-        o.subset(up,p);
+        System.out.println(o.subSet(up,p));
 
     }
 
-    void subset(String up , String p){
+//    void subset(String up , String p){
+//
+//        if(up.isEmpty()){
+//            System.out.println(p);
+//            return;
+//        }
+//
+//        char ch = up.charAt(0);
+//
+//        subset(up.substring(1),p+ch);
+//        subset(up.substring(1),p);
+//    }
+    //Adding in array List By creating list inside Body of the function
+    static ArrayList<String> subSet(String up , String p){
 
         if(up.isEmpty()){
-            System.out.println(p);
-            return;
+
+            ArrayList<String> list = new ArrayList<>();
+
+            list.add(p);
+
+            return list;
         }
+
 
         char ch = up.charAt(0);
 
-        subset(up.substring(1),p+ch);
-        subset(up.substring(1),p);
+        ArrayList<String> left =  subSet(up.substring(1),p+ch);
+        ArrayList<String> right =  subSet(up.substring(1),p);
 
+        left.addAll(right);
 
+        return left;
 
 
     }
