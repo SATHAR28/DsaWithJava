797e55239722801159a2a911ed71252a5300e4a7
SATHAR28 <sat20006@gmail.com>
2026-09-11 20:17:31 +0530

    Permutation of string done using recurssion

---
+++ 2DBinarySearch/Recursion permutation/src/permutaionOfString.java
@@ -0,0 +1,26 @@
+public class permutaionOfString {
+    public static void main(String[] args) {
+
+        String up = "abc";
+        String p ="";
+        permutation(p,up);
+    }
+
+    static void permutation(String p , String up){
+
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        for(int i = 0 ; i <= p.length();i++){
+
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+
+            permutation(f+ch+s,up.substring(1));
+        }
+    }
+}
